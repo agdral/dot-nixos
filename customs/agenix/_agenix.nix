e82da2a8a12bdc0abeb26c123bd6c9cdc7b22348
agdral <ezequiel.escobar@proton.me>
2026-03-07 18:52:15 -0300

    remove postinstall check of agenix

--- customs/agenix/_agenix.nix
+++ customs/agenix/_agenix.nix
@@ -26,23 +26,6 @@ stdenv.mkDerivation rec {
   dontUnpack = true;
   doInstallCheck = true;
   installCheckInputs = [ shellcheck ];
-  postInstallCheck = ''
-    shellcheck ${bin}
-    ${bin} -h | grep ${version}
-
-    test_tmp=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
-    export HOME="$test_tmp/home"
-    export NIX_STORE_DIR="$test_tmp/nix/store"
-    export NIX_STATE_DIR="$test_tmp/nix/var"
-    mkdir -p "$HOME" "$NIX_STORE_DIR" "$NIX_STATE_DIR"
-    function cleanup {
-      rm -rf "$test_tmp"
-    }
-    trap "cleanup" 0 2 3 15
-
-    cd $HOME/secrets
-    test $(${bin} -d secret1.age) = "hello"
-  '';
 
   installPhase = ''
     install -D $src ${bin}
