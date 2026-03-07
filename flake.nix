2784295c4b28e51127f1ff4c5dc69622a04a4e2c
agdral <ezequiel.escobar@proton.me>
2026-03-06 22:47:30 -0300

    add agenix

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     import-tree.url = "github:vic/import-tree";
+    agenix.url = "github:ryantm/agenix";
 
     nixstable.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
 
@@ -17,15 +18,21 @@
     nixpkgs,
     import-tree,
     solaar,
+    agenix,
     ...
   }: let
     lib = nixpkgs.lib;
   in {
     nixosModules.default = {...}: {
       imports = [
+        agenix.nixosModules.default
         solaar.nixosModules.default
         ./packages
         (import-tree.filter (lib.hasSuffix "/default.nix") ./services)
+      ];
+
+      environment.systemPackages = [
+        agenix.packages.x86_64-linux.default
       ];
     };
 
